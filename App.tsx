4990a0732c7bfcd84cd20f39f8aa94844c893438
alvinotuya84 <alvinotuya8@gmail.com>
2024-06-18 06:17:08 +0300

    amega group initialization

---
+++ App.tsx
@@ -0,0 +1,43 @@
+import 'react-native-gesture-handler';
+import {store} from '@/src/app/store';
+import SplashScreen from '@/src/components/SplashScreen';
+import Navigation from '@/src/navigation';
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQueryClient,
+} from '@tanstack/react-query';
+import {useEffect, useState} from 'react';
+import {Platform, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {Provider} from 'react-redux';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+const App = () => {
+  const [splashScreenVisible, setSplashScreenVisible] = useState(true);
+
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Navigation />
+
+          {/* {splashScreenVisible && (
+            <SplashScreen
+              onAnimationEnd={() => {
+                setSplashScreenVisible(false);
+              }}
+            />
+          )} */}
+        </View>
+      </Provider>
+    </QueryClientProvider>
+  );
+};
+export default App;
---
+++ src/configs/colors.configs.ts
@@ -0,0 +1,31 @@
+export const Colors = {
+  common: {
+    primary: '#1E88E5',
+    secondary: '#000',
+    tertiary: '#000',
+    quaternary: '#000',
+    quinary: '#000',
+    senary: '#000',
+  },
+  light: {
+    primary: '#1E88E5',
+    border: 'rgb(39, 39, 41)',
+    surface: '#F6F7FB',
+    text: '#000',
+    stroke: '#000',
+    background: '#fff',
+    danger: '#FF0000',
+    success: '#009246',
+  },
+  dark: {
+    primary: '#1E88E5',
+    border: 'rgb(39, 39, 41)',
+    surface: '#F6F7FB',
+
+    text: '#fff',
+    danger: '#FF0000',
+    background: '#000',
+    stroke: '#fff',
+    success: '#009246',
+  },
+};
